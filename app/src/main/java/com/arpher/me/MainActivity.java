502ac31991c91206b3ce82c03f8b215a8c112f48
arpher <arpher@qq.com>
2016-12-18 09:51:34 +0800

    6.梵高的自画像

--- app/src/main/java/com/arpher/me/MainActivity.java
+++ app/src/main/java/com/arpher/me/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +21,30 @@ public class MainActivity extends AppCompatActivity {
         // attach view tree to the activity
         setContentView(R.layout.activity_main);
 
+        sharedPref = this.getSharedPreferences("com.arpher.me",
+                Context.MODE_PRIVATE);
+
         // monitor "click" action for the button
         Button btn = (Button) findViewById(R.id.author);
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,
-                        getString(R.string.author),
-                        Toast.LENGTH_LONG).show();
+//                Toast.makeText(MainActivity.this,
+//                        getString(R.string.author),
+//                        Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, SelfEditActivity.class);
+                MainActivity.this.startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TextView nameView = (TextView) findViewById(R.id.welcome);
+
+        // retrieve content from shared preference, with key "name"
+        String   welcome  = "Welcome, " + sharedPref.getString("name", "unknown") + "!";
+        nameView.setText(welcome);
     }
 }
